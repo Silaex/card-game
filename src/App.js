dcdfc932b693474af9be3230971530b972799bf2
Silaex <alexis.provot45@gmail.com>
2019-12-16 01:03:41 +0100

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card/Card';
 
 function App() {
+
+  const [choosen, setChoosen] = useState("Bruiorian")
+
+  const checkBackground = () => {
+    const backgrounds = {
+      "Landiarian": require('./components/images/background.png'),
+      "Bruiorian": require('./components/images/orc_background.png'),
+      "Rakalvian": require('./components/images/troll_background.png')
+    }
+    return backgrounds[choosen]
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ background: `url(${checkBackground()})`}}>
+      <div className="dark-overlay"></div>
+      <Card name="Landiarian" choosen={choosen} pick={setChoosen} />
+      <Card name="Bruiorian" choosen={choosen} pick={setChoosen} />
+      <Card name="Rakalvian" choosen={choosen} pick={setChoosen} />
     </div>
   );
 }
